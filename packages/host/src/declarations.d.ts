96c11b1ec487409370e7a2d4992cc751c731df3e
Wolf-Briones <fbrionesp93@gmail.com>
2026-06-14 17:38:56 -0500

    chore: 140626FB fix auth providers ejecution time

---
+++ packages/host/src/declarations.d.ts
@@ -0,0 +1,16 @@
+declare module 'auth/AuthProvider' {
+  import {ComponentType} from 'react';
+
+  const AuthProvider: ComponentType<any>;
+
+  export default AuthProvider;
+}
+
+
+declare module 'auth/SignInScreen' {
+  import {ComponentType} from 'react';
+
+  const SignInScreen: ComponentType<any>;
+
+  export default SignInScreen;
+}
